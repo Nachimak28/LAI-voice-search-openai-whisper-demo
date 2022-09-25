a203add42a51fbc16e33c7b1fa7242d08b5038f8
Aniket Maurya <theaniketmaurya@gmail.com>
2022-09-25 11:55:08 +0530

    add mic

--- LAI_voice_search_openai_whisper_demo/components/web_ui.py
+++ LAI_voice_search_openai_whisper_demo/components/web_ui.py
@@ -5,7 +5,6 @@ from lightning_app.components.serve import ServeGradio
 from lightning_app import BuildConfig
 import gradio as gr
 from .openai_whisper_demo import WhisperSearch
-# from .ddg_search_component import Search
 
 
 @dataclass
@@ -16,23 +15,25 @@ class CustomBuildConfig(BuildConfig):
 
 class LitGradio(ServeGradio):
 
-    inputs = gr.components.Audio(source="upload", type="filepath", label='Record your voice here')
-    # inputs = gr.components.Textbox(label='Keyword Entry')
-    outputs = gr.components.HTML(label='output')
-    enable_queue = True
-    # examples = [['resources/']]
+    inputs = [
+        gr.components.Audio(
+            source="microphone", type="filepath", label="Record your voice"
+        ),
+        gr.components.Audio(
+            source="upload", type="filepath", label="Upload your audio"
+        ),
+    ]
+    outputs = gr.components.HTML(label="Transcribed output")
+    enable_queue = False
 
     def __init__(self):
         # Use the custom build config
-        super().__init__(parallel=True, cloud_build_config = CustomBuildConfig())
-        os.system(f"yes {os.environ['RS_PASS']} | sudo passwd zeus")
-        os.system("sudo apt-get update")
-        os.system("sudo apt-get install -y ffmpeg")
+        super().__init__(parallel=True, cloud_build_config=CustomBuildConfig())
 
-    def predict(self, audio_file):
+    def predict(self, recorded_audio, uploaded_audio):
+        audio_file = recorded_audio or uploaded_audio
         return self.model.get_search_results_from_speech(audio_file)
 
     def build_model(self):
         voice_to_search = WhisperSearch()
         return voice_to_search
-
